b7d717d3ef0a21ad0687c13b98a09bae032c315b
arpan77249 <arpankochale054@gmail.com>
2025-06-29 17:49:58 +0000

    done

--- Javascript/one.js
+++ Javascript/one.js
@@ -1,1 +1,69 @@
-console.log("Arpan")
+// console.log("Arpan") 
+// +++++++ start basic++++++++++
+const accountId = 1221
+ let email = "arpan@gmail.com"
+var accountpassword="2222"
+accountCity ="indore"
+// console.log(accountCity)
+
+// -----data type------
+// number
+// bigInt (big data
+// string 
+// boolean(0,1) (true or false)
+// null 
+// undefind
+// Symbol
+// console.log (typeof datatype) //this is the definde the data type
+//  console.log (typeof Undefinde)
+//  console.log (typeof null)
+
+
+// +++++++++++ Datatype conversion+++++++++++
+//tow type of datatype is : primitive and nonprimitve
+// ++++++++++= primitive ++++++++
+
+let IslogedIn = 0
+let BooleanIslogedIn = Boolean(IslogedIn)
+// console.log( BooleanIslogedIn);
+
+
+// +++++++++ oprator +++++++
+
+// +,-,*,/,(**), % and assignment oprator 
+// string addition 
+
+let str1 = "arpan"
+let str2 = "kochale"
+let str3 = str1 + str2
+// console.log(str3);
+
+// ++++++++++++++ Memory ++++++++++
+// tow type memory is : STACK AND HEAP 
+
+
+// --------- string in java script ---------
+const Name = "arpan"
+const account = 50 
+
+console.log( ` hello my name is ${Name} and my account value is ${account}:`);
+
+
+
+
+
+
+
+// ++++++++++ Non primitive +++++++++++
+// Array , object , function
+
+// ----Array-----
+ 
+const arr1 = [1,2,4,5,3]
+console.log(arr1);
+
+
+
+
+
+
